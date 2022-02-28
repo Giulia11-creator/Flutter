c1bac08c8a746a74dc7551fe0cb1505f012f7529
Giulia11 <bertoligiulia.kennedy@gmail.com>
2022-02-28 09:46:51 +0100

    QuizLuke

--- FlutterExample-quiz/lib/textdisplay.dart
+++ FlutterExample-quiz/lib/textdisplay.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:html/parser.dart' show parse;
+import 'package:html/dom.dart' as htmlParser;  // to print html chars
 
 class TextDisplay extends StatelessWidget {
   final String myText;
@@ -11,7 +15,7 @@ class TextDisplay extends StatelessWidget {
       width: double.infinity,
       margin: EdgeInsets.all(10),
       child: Text(
-        myText,
+        htmlParser.DocumentFragment.html(myText).text.toString(),
         style: TextStyle(fontSize: 28),
         textAlign: TextAlign.center,
       ),
